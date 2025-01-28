59bc8f686a5e5d1a033fdc09c7b27e8fc75004a1
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2025-01-28 15:07:31 -0400

    Correção nos códigos aula Spring Security.

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,15 +2,10 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
-import med.voll.api.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
-@Service
-public class AutenticacaoController implements UserDetailsService {
+public class AutenticacaoController{
 
     @Autowired
     private AuthenticationManager manager;
@@ -32,15 +26,4 @@ public class AutenticacaoController implements UserDetailsService {
         return ResponseEntity.ok().build();
     }
 
-    @Autowired
-    private UsuarioRepository repository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDetails user = repository.findByLogin(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        return user;
-    }
 }
--- src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
+++ src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -14,6 +14,10 @@ public class AutenticacaoService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByLogin(username);
+        UserDetails user = repository.findByLogin(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
     }
 }
